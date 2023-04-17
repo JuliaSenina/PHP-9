66680dc19b4951a5cdd28f5d3b56e754bea4d007
Сенина Юлия Александровна <y.senina@ens.ru>
2023-04-17 12:48:34 +0300

    commit

---
+++ web/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Form</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <form class="box-info" action="post.php" method="POST">
+    <label for="user_name">Ваше имя</label>
+    <input id="user_name" type="text" name="user_name" />
+    <input type="submit" name="submit" value="Войти" />
+  </form>
+</body>
+
+</html>
